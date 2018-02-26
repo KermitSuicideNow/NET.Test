9c1ae4d4a05e7ddd7e86652c598183e63370b9a7
Erick Orlando <erickorlando@outlook.com>
2018-02-26 08:18:19 -0500

    Ejemplo de VB.NET actualizado

---
+++ Clientes OpenInvoicePeru/OpenInvoicePeru.ApiClientVisualBasic/RestHelper.vb
@@ -0,0 +1,17 @@
+﻿Imports RestSharp
+
+Public Class RestHelper(Of TRequest As Class, TResponse As {Class, New})
+    Public Shared Function Execute(metodo As String, request As TRequest) As TResponse
+        Dim client As New RestClient("http://localhost/OpenInvoicePeru/api")
+
+        Dim restRequest As New RestRequest(metodo, Method.POST) With { _
+                .RequestFormat = DataFormat.Json _
+                }
+
+        restRequest.AddBody(request)
+
+        Dim restResponse = client.Execute(Of TResponse)(restRequest)
+        Return restResponse.Data
+    End Function
+
+End Class
